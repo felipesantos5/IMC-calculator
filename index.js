41a1faf6c34408f7f2d49c91f31a77e343d60df7
felipesantos5 <felipesantosmarcelino2004@gmail.com>
2022-05-25 03:18:16 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+function calculate () {
+  var height=document.getElementById("height").value
+  var weight=document.getElementById("weight").value
+
+  var imc = weight / height ** 2;
+
+  var text=""
+  if(imc<18.5){
+    text="Você esta abaixo do peso ideal"
+  } else if(imc<24.9){
+    text="Você esta no peso ideal"
+  } else if(imc<29.9){
+    text="Você está com sobrepeso"
+  } else if(imc<39.9){
+    text="Você está com obesidade"
+  } else if(imc>39.9){
+    text="Você está com obesidade mórbida"
+  }
+
+  document.getElementById("text_area").innerText = text
+
+}
